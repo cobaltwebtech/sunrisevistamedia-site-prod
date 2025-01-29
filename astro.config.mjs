27868dfc74d5c8afd537d3c0bbdb3d187a5a9958
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2025-01-28 21:13:39 -0600

    Pushed component changes and updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,45 @@ export default defineConfig({
   output: 'server',
   integrations: [
     tailwind(), 
-    icon(),
+    icon({
+      include: {
+        'fa-brands': [
+          'facebook',
+          'vimeo',
+          'instagram'
+        ],
+        fluent: [
+          'people-chat-24-filled'
+        ],
+        'fluent-color': [
+          'camera-24',
+          'error-circle-24',
+          'video-24'
+        ],
+        mdi: [
+          'arrow-up-drop-circle',
+          'envelope', 
+          'send', 
+          'connect-without-contact',
+          'information-slab-circle',
+          'film-open-check',
+          'movie-edit',
+          'bullhorn',
+          'youtube-tv',
+          'arrow-right',
+          'home',
+          'email-check'
+        ],
+        mingcute: [
+          'video-camera-fill',
+          'menu-fill',
+          'close-fill',
+          'sun-fill',
+          'moon-stars-fill',
+          'arrow-right-fill'
+        ]
+      }
+    }),
     sitemap({
       includeByDefault: true,
     }),
